15fab473bfe77ee592c638fbfdc519f312c6425b
Stuart Mumford <stuart@cadair.com>
2016-03-22 13:55:04 -0700

    Add astropy_helpers for basic packaging

--- astrospam/__init__.py
+++ astrospam/__init__.py
@@ -1,4 +1,15 @@
-"""This is the ``__init__.py`` docstring.
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
 """
-from .spam import *
-from .ham import *
+This is an Astropy affiliated package.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from astrospam import *
